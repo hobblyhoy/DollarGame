e3eb69b279f871f90b71dbeaf9cfa4ac18fb3ecd
hobblyhoy <hobblyhoy@gmail.com>
2018-11-19 21:47:02 -0500

    base html / js files

---
+++ dollarGameScript.js
@@ -0,0 +1,25 @@
+"use strict";
+/* global document,window,console,requestAnimationFrame*/
+/* jshint -W097,-W014 */ //turn off use strict and comma styling warnings
+
+// Canvas init
+var canvas = document.querySelector('canvas');
+var canvasWidth = window.innerWidth-2;
+var canvasHeight = window.innerHeight-2;
+canvas.width = canvasWidth;
+canvas.height = canvasHeight;
+var c = canvas.getContext('2d');
+
+
+// Game objects
+
+
+// Helper Utils
+
+
+// Animation Loop
+
+
+// Event Listeners
+
+
