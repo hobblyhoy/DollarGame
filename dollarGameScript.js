8ae94421b8a0f3fa154dde758fef326c51be61a2
hobblyhoy <hobblyhoy@gmail.com>
2018-11-20 23:33:21 -0500

    wip save playing around with mouse positions

--- dollarGameScript.js
+++ dollarGameScript.js
@@ -14,9 +14,52 @@ canvas.width = container.offsetWidth - paddingPixelBuffer;
 canvas.height = container.offsetHeight - paddingPixelBuffer;
 canvas.style.border = paddingPixelBuffer/2 + 'px solid black';
 canvas.style.display = 'block';
+// Location
+// canvasPosition > (Absolute/Relative) > (Start/Mid/End) > (X/Y)
+var canvasPosition = {
+    // absolute: {
+    //     top: {
+    //         left: Math.floor(canvasBoundingRect.left)
+    //         , center: adf
+    //         , right: Math.floor(canvasBoundingRect.top)
+    //     }, center: {
+    //         left: Math.floor((canvasBoundingRect.right - canvasBoundingRect.left)/2)
+    //         , center: asdf
+    //         , right: Math.floor((canvasBoundingRect.bottom - canvasBoundingRect.top)/2)
+    //     }, end: {
+};
 
-var canvasStartX = Math.floor(canvasBoundingRect.left);
-var canvasStartY = Math.floor(canvasBoundingRect.top);
+var xPositions = {
+    top: 0
+    , center: canvas.height/2
+    , bottom: canvas.height
+};
+var yPositions = {
+    left: 0
+    , center: canvas.width/2
+    , right: canvas.width
+};
+var toAbsolute = function (obj) {
+    if (obj.x === undefined || obj.y === undefined) throw 'bad obj'
+    return {
+        x: obj.x + Math.floor(canvasBoundingRect.top)
+        , y: obj.y + Math.floor(canvasBoundingRect.left)
+    };
+};
+var toRelative = //stub
+
+for (var xKey in xPositions) {
+    for (var yKey in yPositions) {
+        if (!canvasPosition[xKey]) canvasPosition[xKey] = {};
+        var relativeCoordinates = {
+            x: xPositions[xKey]
+            ,y: yPositions[yKey]
+        };
+        canvasPosition[xKey][yKey] = relativeCoordinates;
+        canvasPosition[xKey][yKey].toAbsolute = toAbsolute.bind(this);
+
+    }
+}
 
 
 //-- Constants
@@ -28,7 +71,7 @@ var largeRadius = 40;
 
 //-- Game State
 var vertices = [];
-var mouseLoc = {x:null, y: null}; // last known mouse location
+var mouseLoc = {x:null, y: null}; // last known mouse location relative to canvas
 
 
 //-- Game objects
@@ -58,9 +101,10 @@ var Vertex = function(x, y) {
 
 
 //-- Game Init
-vertices.push(new Vertex(0,0));
-vertices.push(new Vertex(100,100));
-vertices.push(new Vertex(300,300));
+vertices.push(new Vertex(0, 0));
+vertices.push(new Vertex(100, 100));
+// vertices.push(new Vertex(canvasPosition.relative.mid.x, canvasPosition.relative.mid.y));
+// vertices.push(new Vertex(canvasPosition.relative.end.x, canvasPosition.relative.end.y));
 
 
 //-- Game Logic & Animation Loop
@@ -79,8 +123,9 @@ animationLoop();
 //-- Event Listeners
 window.addEventListener('mousemove', function(event) {
     // event gives x/y coordinate on the page but we need it relative to the canvas
-    mouseLoc.x = event.x - canvasStartX;
-    mouseLoc.y = event.y - canvasStartY;
+    var absolutePos = toRelative(event);
+    mouseLoc.x = absolutePos.x;
+    mouseLoc.y = absolutePos.y;
 
     //console.log('(' + mouseLoc.x + '),(' + mouseLoc.y + ')');
 });
